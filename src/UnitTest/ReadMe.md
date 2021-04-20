94a1c4c137c85898a71c5fd10b4bd4ab1c471277
JulYred <zhusanshi30@gmail.com>
2021-04-21 00:20:20 +0800

    Flaui Create

---
+++ src/UnitTest/ReadMe.md
@@ -0,0 +1,5 @@
+# 说明
+ 
+练习使用FlaUI对WPF UI 进行测试
+
+https://github.com/FlaUI/FlaUI
---
+++ src/UnitTest/WpfDemoTest/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Conditions;
+using FlaUI.UIA3;
+
+namespace WpfDemoTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            TestSomething();
+        }
+
+        private void TestSomething()
+        {
+            var app = FlaUI.Core.Application.Launch(@"C:\Users\zhusa\Documents\GitHub\JulYredSolution\src\UnitTest\WpfDemo\bin\Debug\net5.0-windows\WpfDemo.exe");
+            using (var automation = new UIA3Automation())
+            {
+                var window = app.GetMainWindow(automation);
+                ConditionFactory cf = new ConditionFactory(new UIA3PropertyLibrary());
+
+                var dd = window.FindAllChildren().FirstOrDefault(a => a.Name == "Click");
+                dd?.Click();
+            }
+        }
+
+
+    }
+}
